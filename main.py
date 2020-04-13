e177d2f57339528f963862020746177829a395a8
Breno Poggiali <brenopoggiali@gmail.com>
2020-04-13 00:43:35 -0300

    Add internships table

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+import os
 from sitemap import get_jobs
+from tabulate import tabulate
 from jobs import get_internships, parse_jobs
 
 
-def print_internships_by_country(internships):
+def clean_screen():
+    return os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def get_terminal_link(url):
+    text = "Link"
+    terminal_link = f"\u001b]8;;{url}\u001b\\{text}\u001b]8;;\u001b\\"
+    return terminal_link
+
+
+def get_countries(locations):
+    countries = [i[0] for i in locations]
+    countries = sorted(set(countries))
+    return ', '.join(countries)
+
+
+def print_internships(internships):
+    table = []
+    for internship in internships:
+        url = internship['loc']
+        title = internship['title']
+        countries = get_countries(internship['locations'])
+        link = get_terminal_link(url)
+        table.append([title,  countries, link])
+    table = sorted(table, key=lambda x: x[1])
+    print(tabulate(table, headers=['Title', 'Countries', 'Link']))
     return
 
 
@@ -12,4 +39,5 @@ if __name__ == '__main__':
     internships_urls = get_internships(jobs)
     print(f"Parsing {len(internships_urls)} internships... ")
     internships = parse_jobs(internships_urls)
-    # print_internships_by_country(internships)
+    clean_screen()
+    print_internships(internships)
